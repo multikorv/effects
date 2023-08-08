c76046229b23be2f7b7e4ef6af3963d54edda97e
Marcus Antonsson <multikorv@users.noreply.github.com>
2023-08-08 20:10:59 +0200

    Remove vulkan implementation

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,3 @@
-extern crate vulkano;
-
-use std::sync::Arc;
-
-use vulkano::{
-    VulkanLibrary,
-    instance::{
-        Instance,
-        InstanceCreateInfo
-    },
-    device::{
-        Device,
-        DeviceCreateInfo,
-        QueueCreateInfo,
-        QueueFlags,
-        DeviceExtensions,
-        physical::{
-            PhysicalDevice,
-            PhysicalDeviceType
-        }, Queue
-    },
-    swapchain::{
-        Swapchain,
-        SwapchainCreateInfo, self, SwapchainPresentInfo,
-    },
-    image::{ImageUsage, SwapchainImage, view::ImageView},
-    buffer::BufferContents, render_pass::{RenderPass, Framebuffer, Subpass}, single_pass_renderpass, shader::ShaderModule, pipeline::{graphics::{viewport::{Viewport, ViewportState}, vertex_input::{BuffersDefinition, Vertex, VertexBufferDescription}, input_assembly::InputAssemblyState}, GraphicsPipeline}, command_buffer::{PrimaryAutoCommandBuffer, AutoCommandBufferBuilder, CommandBufferUsage, RenderPassBeginInfo, SubpassContents}
-
-};
-use vulkano_win::VkSurfaceBuild;
 use winit::{
     window::WindowBuilder,
     event_loop::{
@@ -40,76 +10,13 @@ use winit::{
     }
 };
 
-
 fn main() {
-    let library: Arc<VulkanLibrary> = VulkanLibrary::new()
-        .expect("No vulkan library/DLL");
-
-    let required_extensions = vulkano_win::required_extensions(&library);
-
-    let instance = Instance::new(
-        library,
-        InstanceCreateInfo {
-            enabled_extensions: required_extensions,
-            ..Default::default()
-        }
-    )
-    .expect("Failed to create instance");
 
     let event_loop = EventLoop::new();
 
-    let surface = WindowBuilder::new()
-        .build_vk_surface(&event_loop, instance.clone())
+    let window = WindowBuilder::new()
+        .build(&event_loop)
         .unwrap();
-
-    let device_extensions = DeviceExtensions {
-        khr_swapchain: true,
-        ..DeviceExtensions::empty()
-    };
-
-    let (physical_device, queue_family_index) = select_physical_device(&instance, &surface, &device_extensions);
-
-    let (device,  mut queues) = Device::new(
-        physical_device.clone(),
-        DeviceCreateInfo {
-            queue_create_infos: vec![QueueCreateInfo{
-                queue_family_index,
-                ..Default::default()
-            }],
-            enabled_extensions: device_extensions,
-            ..Default::default()
-        }
-    )
-    .unwrap();
-
-    let caps = physical_device
-        .surface_capabilities(&surface, Default::default())
-        .expect("Failed to get surface capabilities");
-
-    let composite_alpha = caps.supported_composite_alpha.into_iter().next().unwrap();
-
-    let image_format = Some(
-        physical_device
-            .surface_formats(&surface, Default::default())
-            .unwrap()[0]
-            .0,
-    );
-
-    let image_extent = caps.current_extent.unwrap_or([640, 480]);
-
-    let (swapchain, images) = Swapchain::new(
-        device.clone(),
-        surface.clone(),
-        SwapchainCreateInfo {
-            min_image_count: caps.min_image_count + 1,
-            image_format,
-            image_extent,
-            image_usage: ImageUsage::COLOR_ATTACHMENT,
-            composite_alpha,
-            ..Default::default()
-        },
-    )
-    .unwrap();
 
     event_loop.run(|event, _, control_flow| {
         match event {
@@ -119,94 +26,4 @@ fn main() {
             _ => ()
         }
     });
-}
-
-
-fn select_physical_device(instance: &Arc<Instance>, surface: &Arc<vulkano::swapchain::Surface>, device_extensions: &DeviceExtensions) -> (Arc<PhysicalDevice>, u32) {
-    instance
-        .enumerate_physical_devices()
-        .expect("Could not enumerate devices")
-        .filter(|p| {
-            p.supported_extensions().contains(device_extensions)})
-        .filter_map(|p| {
-            p.queue_family_properties()
-                .iter()
-                .enumerate()
-                .position(|(i, q)| {
-                    q.queue_flags.contains(QueueFlags::GRAPHICS) &&
-                    p.surface_support(i as u32, &surface).unwrap_or(false)
-                })
-                .map(|q| (p, q as u32))
-        })
-        .min_by_key(|(p, _)| {
-            match p.properties().device_type {
-                PhysicalDeviceType::DiscreteGpu => 0,
-                PhysicalDeviceType::IntegratedGpu => 1,
-                PhysicalDeviceType::VirtualGpu => 2,
-                PhysicalDeviceType::Cpu => 3,
-                _ => 4,
-            }
-        })
-        .expect("No devices available")
-}
-
-fn get_render_pass(device: Arc<Device>, swapchain: &Arc<Swapchain>) -> Arc<RenderPass> {
-    vulkano::single_pass_renderpass!(
-        device,
-        attachments: {
-            color: {
-                load: Clear,
-                store: Store,
-                format: swapchain.image_format(),
-                samples: 1
-            },
-        },
-        pass: {
-            color: [color],
-            depth_stencil: {},
-        },
-    )
-    .unwrap()
-}
-
-fn get_framebuffers(images: &[Arc<SwapchainImage>], render_pass: &Arc<RenderPass>) -> Vec<Arc<Framebuffer>> {
-    images
-        .iter()
-        .map(|image| {
-            let view = ImageView::new_default(image.clone()).unwrap();
-            Framebuffer::new(
-                render_pass.clone(),
-                vulkano::render_pass::FramebufferCreateInfo {
-                    attachments: vec![view],
-                    ..Default::default()
-                },
-            )
-            .unwrap()
-        })
-        .collect::<Vec<_>>()
-}
-
-fn get_pipeline(
-    device: Arc<Device>,
-    vs: Arc<ShaderModule>,
-    fs: Arc<ShaderModule>,
-    render_pass: Arc<RenderPass>,
-    viewport: Viewport,
-) -> Arc<GraphicsPipeline> {
-    GraphicsPipeline::start()
-        .vertex_input_state(BuffersDefinition::new())
-        .vertex_shader(vs.entry_point("main").unwrap(), ())
-        .input_assembly_state(InputAssemblyState::new())
-        .viewport_state(ViewportState::viewport_fixed_scissor_irrelevant([viewport]))
-        .fragment_shader(fs.entry_point("main").unwrap(), ())
-        .render_pass(Subpass::from(render_pass, 0).unwrap())
-        .build(device)
-        .unwrap()
-}
-
-#[derive(BufferContents)]
-#[repr(C)]
-struct BufferObject {
-    foo: u32,
-    bar: u32
 }
