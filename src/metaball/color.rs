5686652583dbf74a46b012df1914594cb31a565e
Marcus Antonsson <multikorv@users.noreply.github.com>
2023-08-17 19:22:58 +0200

    Add predefined basic colors in color module

--- src/metaball/color.rs
+++ src/metaball/color.rs
@@ -15,3 +15,33 @@ impl From<Color> for u32 {
         color.blue | color.green << 8 | color.red << 16
     }
 }
+
+const WHITE: Color = Color {
+    red:255,
+    green: 255,
+    blue: 255
+};
+
+const BLACK: Color = Color {
+    red:0,
+    green: 0,
+    blue: 0
+};
+
+const RED: Color = Color {
+    red:255,
+    green: 0,
+    blue: 0
+};
+
+const GREEN: Color = Color {
+    red: 0,
+    green: 255,
+    blue: 0
+};
+
+const BLUE: Color = Color {
+    red:0,
+    green: 0,
+    blue: 255
+};
