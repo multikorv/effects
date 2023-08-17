7d46d2b7a23d5bed7927c551e13dad6dd649b80d
Marcus Antonsson <multikorv@users.noreply.github.com>
2023-08-17 19:28:30 +0200

    Implement Default for Ball and change some accessability

--- src/metaball/ball.rs
+++ src/metaball/ball.rs
@@ -1,8 +1,13 @@
-#[derive(Debug, Default)]
+use crate::metaball::{
+    vec3::Vec3,
+    color::Color
+};
+
+#[derive(Debug)]
 pub struct Ball {
-    position: Vec3,
-    radius: u32,
-    color: Color
+    pub position: Vec3,
+    pub radius: u32,
+    pub color: Color
 }
 
 impl Ball {
@@ -11,6 +16,16 @@ impl Ball {
             position,
             radius,
             color
+        }
+    }
+}
+
+impl Default for Ball {
+    fn default() -> Self {
+        Self {
+            position: Default::default(),
+            radius: 10,
+            color: Default::default()
         }
     }
 }
