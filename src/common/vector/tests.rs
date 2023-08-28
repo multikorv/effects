e929cc6c790db459863a2139a0ef3acf983bd309
Marcus Antonsson <multikorv@users.noreply.github.com>
2023-08-28 20:24:09 +0200

    Update vec2 unit test to run since they changed from a vec3 to a vec2

--- src/common/vector/tests.rs
+++ src/common/vector/tests.rs
@@ -2,12 +2,12 @@ use crate::common::vector::Vec2;
 
 #[test]
 fn given_right_hand_side_is_smaller_when_subtracti_then_result_should_be_2() {
-    let result: Vec2 = Vec2::new(4, 4, 4) - Vec2::new(2, 2, 2);
-    assert_eq!(result, Vec2::new(2, 2, 2))
+    let result: Vec2 = Vec2::new(4.0, 4.0) - Vec2::new(2.0, 2.0);
+    assert_eq!(result, Vec2::new(2.0, 2.0))
 }
 
 #[test]
 fn given_right_hand_side_is_larger_when_subtract_then_result_should_be_negative_2() {
-    let result: Vec2 = Vec2::new(4, 4, 4) - Vec2::new(6, 6, 6);
-    assert_eq!(result, Vec2::new(-2, -2, -2))
+    let result: Vec2 = Vec2::new(4.0, 4.0) - Vec2::new(6.0, 6.0);
+    assert_eq!(result, Vec2::new(-2.0, -2.0))
 }
