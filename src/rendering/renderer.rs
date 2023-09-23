c79ca193a83654f9fbbc5cd847a89628bbf24866
Marcus Antonsson <multikorv@users.noreply.github.com>
2023-09-23 09:47:01 +0200

    Add slight bend to circle lines based on distance to midpoint and edge
    This gives the illusion of a sphere

--- src/rendering/renderer.rs
+++ src/rendering/renderer.rs
@@ -53,28 +53,46 @@ impl Renderer {
     fn write_with_asymmetric_lines(&mut self, state: &State, time: &Time) {
         for ball in state.balls.iter() {
             const MAX_THETA: f64 = 180.0;
-            let theta_offset: f64 = 10.0 * (f64::sin(time.elapsed.as_secs_f64()) + 2.0 * f64::sin(time.elapsed.as_secs_f64() * (2.0/3.0)) );
+            let theta_offset: f64 = 8.0 * f64::sin(time.elapsed.as_secs_f64());
             let mut theta: f64 = theta_offset;
-            let theta_increment = 5.0;
+            let theta_increment = 2.0;
 
             while theta <= (MAX_THETA + theta_offset) {
                 let edge = ball.edge_pos_for_angle(theta);
 
-                const TRACE_INCREMENT: f64 = 1.0;
-                let mut trace_offset: f64 = 1.0;
+                const TRACE_INCREMENT: f64 = 5.0;
+                let mut trace_offset: f64 = 0.0;
+
+                let dist_to_mid = vector::distance(&edge, &Vec2::new(edge.x, ball.position.y));
+
+                let direction = match ball.position.x - edge.x {
+                    x if x < 0.0 => 1.0,
+                    x if x > 0.0 => -1.0,
+                    _ => 0.0
+                };
 
                 while trace_offset <= (ball.diameter()).into() {
-                    let nearness_to_circle_edge = trace_offset/(ball.radius as f64);
+                    let nearness_to_circle_edge = trace_offset/(ball.diameter() as f64);
 
-                    let interpolated = vector::lerp(
+                    // Mirror to other side, interpolate to that
+                    let edge_on_opposite_side = Vec2::new(edge.x, edge.y - dist_to_mid*2.0);
+                    let interpolated_pixel = vector::lerp(
                         &edge,
-                        &Vec2::new(edge.x, ball.position.y),
+                        &edge_on_opposite_side,
                         nearness_to_circle_edge
                     );
 
+                    let mut pixel_dist_to_midpoint = 1.0 - (vector::distance(&interpolated_pixel, &ball.position) / ball.radius as f64);
+                    let mut nearness_to_mid_x = vector::distance(&interpolated_pixel, &Vec2::new(ball.position.x, interpolated_pixel.y)) / ball.radius as f64;
+
+                    const BEND_AMP: f64 = 90.0;
+                    pixel_dist_to_midpoint *= nearness_to_mid_x;
+                    pixel_dist_to_midpoint *= BEND_AMP;
+                    pixel_dist_to_midpoint *= direction;
+
                     self.write_color(
-                        interpolated.x.round() as u32,
-                        interpolated.y.round() as u32,
+                        (interpolated_pixel.x + (pixel_dist_to_midpoint)).round() as u32,
+                        interpolated_pixel.y.round() as u32,
                         &ball.color
                     );
 
